f62bc6df705fcd24a10922a62c1bf54e0d7f531a
jiaxian.yao@eecs.berkeley.edu <jiaxian.yao@eecs.berkeley.edu>
2019-07-17 00:30:18 -0700

    Removed the motion testing page.

--- motion/motion.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(() => {
-  window.addEventListener('deviceorientation', (event) => {
-    $('#1').text(Math.trunc(event.beta));
-    $('#2').text(Math.trunc(event.gamma));
-  });
-});
